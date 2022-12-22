13a0485db28579ec00677d86dd9dfba351b7e906
Obinna Okere <obinna.okere@lagoonhospitals.com>
2022-12-22 19:05:04 +0100

    Added transitioning

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -23,9 +23,21 @@ const SearchItem = () => {
                 You can cancel anytime.
             </span>
         </div>
-        <div className="siDetails">Detail</div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span>Excellent</span>
+                <button>8.9</button>
+            </div>
+            <div className="siDetailTexts">
+                <span className="siPrice">$450</span>
+                <span className="siTaxOp">Includes taxes and fees</span>
+                <button className='siCheckBbutton'>See availability</button>
+            </div>
+        </div>
     </div>
   )
 }
 
 export default SearchItem
+
+
