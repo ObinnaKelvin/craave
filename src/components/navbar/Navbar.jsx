427e8373de0a69fc00a56cc7efc69a55daff2a78
Obinna Okere <obinna.okere@lagoonhospitals.com>
2022-12-27 12:32:36 +0100

    Login button design updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,34 @@
 import "./navbar.css"
+import logo from "../../images/craave_logo.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { Link } from "react-router-dom"
+import { useState } from "react"
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+      setOpen(!open)
+      console.log(open)
+  }
   return (
     <div className="navbar">
         <div className="navContainer">
-            <span className="logo">Craave Logo</span>
+            {/* <span className="logo">Craave Logo</span> */}
+           <Link to="/"><span className="logo"><img src={logo} alt="logo" className="logo-image"/></span></Link>
             <div className="navItems">
-                <button className="navButton">Register</button>
-                <button className="navButton">Login</button>
+                {/* <button className="navButton">Register</button>
+                <button className="navButton">Login</button> */}
+                <div className={`user`} onClick={handleOpen}>
+                  <span><FontAwesomeIcon icon={faUser}  className="user-icon"/></span>
+                  <div className={`dropDown ${open ? 'active': 'inactive'}`}>
+                    <ul>
+                      <li>Register</li>
+                      <li>Login</li>
+                    </ul>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
