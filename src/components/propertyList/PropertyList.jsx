cb4a686cab7535b6579a859e8e5772440f028d32
Obinna Okere <obinna.okere@lagoonhospitals.com>
2022-12-30 01:17:56 +0100

    Mobile view for property section has been added

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -4,11 +4,70 @@ import Villas from '../../images/villas.jpg'
 import Resorts from '../../images/resorts.jpg'
 import Apartments from '../../images/apartments.jpg'
 import Cabins from '../../images/cabins.jpg'
+import { useRef, useEffect, useState } from 'react'
+import {motion} from 'framer-motion';
+
 
 const PropertyList = () => {
+
+    const [width, setWidth] = useState(0);
+	const carousel = useRef();
+	useEffect(() => {
+		console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+		setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+	}, []);
+
+
+
   return (
     <div className='pList'>
-        <div className="pListItem">
+
+        <motion.div ref={carousel}  className="property-carousel" whileTap={{cursor: "grabbing"}}>
+            <motion.div drag="x" dragConstraints={{right:0, left: -width}} className="property-inner-carousel">
+
+                <motion.div className="pListItem">
+                    <img src={Hotels} alt={Hotels} className="pListImg" />
+                    <div className="pListTitles">
+                        <h1>Hotels</h1>
+                        <h2>233 hotels</h2>
+                    </div>
+                </motion.div>
+
+                <motion.div className="pListItem">
+                    <img src={Resorts} alt={Resorts} className="pListImg" />
+                    <div className="pListTitles">
+                        <h1>Resorts</h1>
+                        <h2>233 hotels</h2>
+                    </div>
+                </motion.div>
+
+                <motion.div className="pListItem">
+                    <img src={Cabins} alt={Cabins} className="pListImg" />
+                    <div className="pListTitles">
+                        <h1>Cabins</h1>
+                        <h2>233 hotels</h2>
+                    </div>
+                </motion.div>
+
+                <motion.div className="pListItem">
+                    <img src={Apartments} alt={Apartments} className="pListImg" />
+                    <div className="pListTitles">
+                        <h1>Apartments</h1>
+                        <h2>233 hotels</h2>
+                    </div>
+                </motion.div>
+
+                <motion.div className="pListItem">
+                    <img src={Villas} alt={Villas} className="pListImg" />
+                    <div className="pListTitles">
+                        <h1>Villas</h1>
+                        <h2>233 hotels</h2>
+                    </div>
+                </motion.div>
+
+            </motion.div>
+        </motion.div>
+        {/* <div className="pListItem">
             <img src={Hotels} alt={Hotels} className="pListImg" />
             <div className="pListTitles">
                 <h1>Hotels</h1>
@@ -42,7 +101,7 @@ const PropertyList = () => {
                 <h1>Villas</h1>
                 <h2>233 hotels</h2>
             </div>
-        </div>
+        </div> */}
     </div>
   )
 }
