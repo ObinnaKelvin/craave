83459662a511607b25c69bc82a7b9c9b4bbf638e
Obinna Okere <obinna.okere@lagoonhospitals.com>
2022-12-30 18:49:15 +0100

    swipe updates

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -4,11 +4,76 @@ import Wheatbaker from '../../images/wheatbaker.jpg'
 import Protea from '../../images/protea.jpg'
 import Sheraton from '../../images/sheraton.jpg'
 import Bokku from '../../images/bokku.jpg'
+import { useRef, useEffect, useState } from 'react'
+import {motion} from 'framer-motion';
 
-const FeaturedProperties = () => {
+const FeaturedProperties = () => {    
+    
+    
+    const [width, setWidth] = useState(0);
+    const carousel = useRef();
+    // useEffect(() => {
+    //     console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    //     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    // }, []);
+
+
+
+
   return (
     <div className='fp'>
-        <div className="fpItem">
+
+        <motion.div ref={carousel}  className="featuredp-carousel" whileTap={{cursor: "grabbing"}}>
+            <motion.div drag="x" dragConstraints={{right:0, left: -width}} className="featuredp-inner-carousel">
+
+                <motion.div className="fpItem">
+                    <img src={Bokku} alt="Bokku" className="fpImage" />
+                    <span className="fpName">Bokku Picasa Hotels</span>
+                    <span className="fpCity">Ogba</span>
+                    <span className="fpPrice">$120 per night</span>
+                    <div className="fpRating">
+                        <button>8.9</button>
+                        <span>Excellent</span>
+                    </div>
+                </motion.div>
+
+                <motion.div className="fpItem">
+                    <img src={Wheatbaker} alt="Wheatbaker" className="fpImage" />
+                    <span className="fpName">The Wheatbaker</span>
+                    <span className="fpCity">Ikoyi</span>
+                    <span className="fpPrice">$70 per night</span>
+                    <div className="fpRating">
+                        <button>8.9</button>
+                        <span>Excellent</span>
+                    </div>
+                </motion.div>
+
+                <motion.div className="fpItem">
+                    <img src={Protea} alt="Protea" className="fpImage" />
+                    <span className="fpName">Protea Hotel</span>
+                    <span className="fpCity">Ikeja</span>
+                    <span className="fpPrice">$40 per night</span>
+                    <div className="fpRating">
+                        <button>8.9</button>
+                        <span>Excellent</span>
+                    </div>
+                </motion.div>
+
+                <motion.div className="fpItem">
+                    <img src={Sheraton} alt="Sheraton" className="fpImage" />
+                    <span className="fpName">Sheraton Hotel</span>
+                    <span className="fpCity">Ikoyi</span>
+                    <span className="fpPrice">$200 per night</span>
+                    <div className="fpRating">
+                        <button>8.9</button>
+                        <span>Excellent</span>
+                    </div>
+                </motion.div>
+
+            </motion.div>
+
+        </motion.div>
+        {/* <div className="fpItem">
             <img src={Bokku} alt="Bokku" className="fpImage" />
             <span className="fpName">Bokku Picasa Hotels</span>
             <span className="fpCity">Ogba</span>
@@ -47,7 +112,7 @@ const FeaturedProperties = () => {
                 <button>8.9</button>
                 <span>Excellent</span>
             </div>
-        </div>
+        </div> */}
     </div>
   )
 }
