f08a70846b17030d3252282b97e31710f72fc3bb
Obinna Okere <obinna.okere@lagoonhospitals.com>
2022-12-31 18:45:36 +0100

    footer updates and display setting

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,35 +1,52 @@
 import './footer.css'
+import logo from '../../images/craave_logo_blue.png'
 
 function Footer() {
   return (
     <div className='footer'>
         <div className="fLists">
+
             <ul className="fList">
-                {/* <strong>Support</strong> */}
+                <img src={logo} alt="a logo" className='fLogo' />
+                <br /><br />
+                {/* <li className="fListItem">Help Center</li>
+                <li className="fListItem">COVID-19 Response</li>
+                <li className="fListItem">Report a neighborhood concern</li>
+                <li className="fListItem">Supporting people with disabilities</li>
+                <li className="fListItem">Craave Cover</li> */}
+            </ul>
+            <ul className="fList">
+                <span className='fHeaderText'>Support</span>
+                <br /><br />
                 <li className="fListItem">Help Center</li>
                 <li className="fListItem">COVID-19 Response</li>
-                <li className="fListItem">Cities</li>
-                <li className="fListItem">District</li>
-                <li className="fListItem">Airports</li>
-                <li className="fListItem">Hotels</li>
+                <li className="fListItem">Report a neighborhood concern</li>
+                <li className="fListItem">Supporting people with disabilities</li>
+                <li className="fListItem">Disaster Relief Housing</li>
+                <li className="fListItem">Combating Discrimination</li>
             </ul>
             <ul className="fList">
-                <li className="fListItem">Countries</li>
-                <li className="fListItem">Regions</li>
-                <li className="fListItem">Cities</li>
-                <li className="fListItem">District</li>
-                <li className="fListItem">Airports</li>
-                <li className="fListItem">Hotels</li>
+                <span className='fHeaderText'>Get Help</span>
+                <br /><br />
+                <li className="fListItem">How It Works</li>
+                <li className="fListItem">FAQs</li>
+                <li className="fListItem">Contact</li>
+                <li className="fListItem">Terms</li>
+                <li className="fListItem">Privacy Policy</li>
             </ul>
             <ul className="fList">
-                <li className="fListItem">Countries</li>
-                <li className="fListItem">Regions</li>
-                <li className="fListItem">Cities</li>
-                <li className="fListItem">District</li>
-                <li className="fListItem">Airports</li>
-                <li className="fListItem">Hotels</li>
+                <span className='fHeaderText'>Hosting</span>
+                <br /><br />
+                <li className="fListItem">Craave your home</li>
+                <li className="fListItem">Craave for Hosts</li>
+                <li className="fListItem">Exploring hosting resources</li>
+                <li className="fListItem">Our community forum</li>
+                <li className="fListItem">Host responsibly</li>
+                {/* <li className="fListItem">Hotels</li> */}
             </ul>
             <ul className="fList">
+                <span className='fHeaderText'>Places</span>
+                <br /><br />
                 <li className="fListItem">Countries</li>
                 <li className="fListItem">Regions</li>
                 <li className="fListItem">Cities</li>
